d3a93e991aa9bbc8f96424b35c4368712a0cdef9
bilelrifi <bilelrifi18@gmail.com>
2025-07-28 12:52:28 +0100

    added urls to the cros

--- backend/index.js
+++ backend/index.js
@@ -19,7 +19,9 @@ const corsOptions = {
   origin: [
     'http://localhost:5173',  
     'http://localhost:8080',
-    'https://job-portal-frontend-gamma.apps.ocp.smartek.ae',  
+    'https://job-portal-frontend-gamma.apps.ocp.smartek.ae',
+    'https://job-portal-dev-myapp-frontend-gamma.apps.ocp.smartek.ae',
+    'https://job-portal-prod-myapp-frontend-gamma.apps.ocp.smartek.ae', 
   ],
   credentials:true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
